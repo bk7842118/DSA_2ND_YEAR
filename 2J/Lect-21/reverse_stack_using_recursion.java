efc1f85b6886fcd887b213409caba83b50a8848a
= <=>
2025-03-26 20:27:46 +0530

    DSA

---
+++ 2J/Lect-21/reverse_stack_using_recursion.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class reverse_stack_using_recursion {
+    public static void reverse(Stack<Integer>stack){
+        if(stack.isEmpty()){
+            return;
+        }
+        int x=stack.peek();
+        stack.pop();
+        reverse(stack);
+        stack.insertElementAt(x,0);
+    }
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(1);
+        stack.push(8);
+        stack.push(6);
+        System.out.println(stack);
+        reverse(stack);
+        System.out.println(stack);
+        
+    } 
+}
